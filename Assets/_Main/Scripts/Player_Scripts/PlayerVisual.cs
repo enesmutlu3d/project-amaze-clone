273963071f1169daf30759db198e1baf2caec01d
Enes <enes.mutlu.3d@gmail.com>
2023-06-09 00:35:23 +0300

    Add: Trail to Player

--- Assets/_Main/Scripts/Player_Scripts/PlayerVisual.cs
+++ Assets/_Main/Scripts/Player_Scripts/PlayerVisual.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class PlayerVisual : MonoBehaviour
 {
     [SerializeField] private MeshRenderer _meshRenderer;
+    [SerializeField] private ParticleSystem _trailParticle;
     private Level _level;
 
     private void Start()
     {
         _level = GetComponentInParent<Level>();
         _meshRenderer.material = _level.ballMaterial;
+    }
+
+    private void OnLevelStart()
+    {
+        _trailParticle.Play();
+    }
+
+    private void OnLevelComplete()
+    {
+        _trailParticle.Stop();
+    }
+
+    private void OnEnable()
+    {
+        LevelManager.LevelStart += OnLevelStart;
+        LevelManager.LevelComlete += OnLevelComplete;
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.LevelStart -= OnLevelStart;
+        LevelManager.LevelComlete -= OnLevelComplete;
     }
 }
